6da3dcc7f7cc040cdee781e38295df3bcdecf5c0
Sreeram Akhil <akhilsreeram663@gmail.com>
2026-03-11 18:55:35 +0530

    Add intersection detection for two linked lists
    Implement method to find intersection of two linked lists.

---
+++ linkedlist/intersection_of_two_ll.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+
+        pa=headA
+        pb=headB
+
+        while pa != pb:
+            pa=pa.next if pa else headB
+            pb=pb.next if pb else headA
+        
+        return pa
+
+  #####################logic######################
+  ##so when ever in the two linked list if we find common elements merge those elements
